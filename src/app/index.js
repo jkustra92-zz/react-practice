83a44f9226c6401410768e997e58bb67502f8144
jkustra92 <jkustra92@gmail.com>
2017-03-02 21:36:07 -0500

    creating sample component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,17 @@
-alert("it's aliiiive");
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var TodoComponent = React.createClass({
+  render: function(){
+    return (
+      <h1>Hellooooooo</h1>
+    );
+  }
+});
+
+// put component into html page
+
+ReactDOM.render(
+  <TodoComponent />, 
+  document.getElementById("todo-wrapper")
+)
